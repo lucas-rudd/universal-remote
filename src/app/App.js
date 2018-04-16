155da0fcbe0a0ff9bfa3beabd00c324a0a1fc5ab
Lucas Rudd <lucas.rudd@asurion.com>
2018-04-15 20:45:41 -0500

    Needed to import the Screens differently

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import * as HomeScreen from './screens/HomeScreen/components/HomeScreen';
-import * as PhilipsHueScreen from './screens/PhilipsHueScreen/components/PhilipsHueScreen';
+import HomeScreen from './screens/HomeScreen/components/HomeScreen';
+import PhilipsHueScreen from './screens/PhilipsHueScreen/components/PhilipsHueScreen';
 
 const App = StackNavigator({
     Home: { screen: HomeScreen },
